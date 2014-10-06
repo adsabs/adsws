51a8643ef2c499f929f2de9b48ef434681489770
Roman Chyla <roman.chyla@gmail.com>
2014-10-06 03:37:58 -0400

    Fixed the wrong column for postgresql

--- alembic/versions/33d84dc97ea1_persistent_session_storage_in_database.py
+++ alembic/versions/33d84dc97ea1_persistent_session_storage_in_database.py
@@ -1,21 +1,16 @@
-"""persistent session storage in database
-
-Revision ID: 33d84dc97ea1
-Revises: 51f3b3b5cd5d
-Create Date: 2014-09-09 11:30:32.615000
-
-"""
-
-# revision identifiers, used by Alembic.
-revision = '33d84dc97ea1'
-down_revision = '51f3b3b5cd5d'
-
-from alembic import op
-import sqlalchemy as db
-
-
-def upgrade():
-    op.create_table('session',        db.Column('session_key', db.String(36), nullable=False,                            server_default='', primary_key=True),        db.Column('session_expiry', db.DateTime, nullable=True, index=True),        db.Column('session_object', db.LargeBinary, nullable=True),        db.Column('uid', db.Integer(), nullable=False, index=True)    )
-
-def downgrade():
-    op.drop_table('session')
+"""persistent session storage in database
+
+Revision ID: 33d84dc97ea1
+Revises: 51f3b3b5cd5d
+Create Date: 2014-09-09 11:30:32.615000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '33d84dc97ea1'
+down_revision = '51f3b3b5cd5d'
+
+from alembic import op
+import sqlalchemy as dbdef upgrade():
+    op.create_table('session',        db.Column('session_key', db.String(255), nullable=False,                            server_default='', primary_key=True),        db.Column('session_expiry', db.DateTime, nullable=True, index=True),        db.Column('session_object', db.LargeBinary, nullable=True),        db.Column('uid', db.Integer(), nullable=False, index=True)    )    def downgrade():
+    op.drop_table('session')

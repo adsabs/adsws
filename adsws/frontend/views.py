f78d02cad4fc9bdedf9762d7a8ee16a9a6d15e2f
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-03-24 15:34:37 +0000

    frontend: base implementation

---
+++ adsws/frontend/views.py
@@ -0,0 +1,15 @@
+from flask import current_app
+
+from flask.ext.restful import Resource
+from flask.ext.ratelimiter import ratelimit
+from .utils import scope_func
+
+class StatusView(Resource):
+  decorators = [ratelimit(1000,24*60*60,scope_func=scope_func)]
+  def get(self):
+    return {'app':current_app.name,'status': 'online'}, 200
+
+class GlobalResourcesView(Resource):
+  decorators = [ratelimit(1000,24*60*60,scope_func=scope_func)]
+  def get(self):
+    return {'error','not implemented'}, 404

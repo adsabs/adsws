5aa20fa741e6cdd793946a444ef75545c4d9c507
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-04-27 18:55:58 +0000

    manage: implement cleanup_clients

--- adsws/accounts/manage.py
+++ adsws/accounts/manage.py
@@ -4,7 +4,7 @@ Management commands for account related activities (including oauth)
 
 import datetime
 
-from adsws.modules.oauth2server.models import OAuthToken
+from adsws.modules.oauth2server.models import OAuthToken, OAuthClient
 from adsws.core import db
 from adsws.accounts import create_app
 
@@ -41,5 +41,38 @@ def cleanup_tokens(app=_app):
                              "Database error; rolled back: {0}".format(e))
         app.logger.info("Deleted {0} expired oauth2tokens".format(deletions))
 
+
+@accounts_manager.command
+def cleanup_clients(app=_app, timedelta="days=31"):
+    """
+    Cleans expired oauth2clients that are older than a specified date in the
+    database defined in app.config['SQLALCHEMY_DATABASE_URI']
+    :param app: Flask application to bind on
+    :param timedelta: String representing the datetime.timedelta against which
+            to compare client's last_activity ["days=31"].
+    :type timedelta: basestring
+    :return: None
+    """
+
+    td = {timedelta.split('=')[0]: float(timedelta.split('=')[1])}
+    td = datetime.timedelta(**td)
+
+    with app.app_context():
+        clients = db.session.query(OAuthClient).filter(
+            OAuthClient.last_activity <= datetime.datetime.now()-td
+        ).all()
+        deletions = 0
+
+        for client in clients:
+            db.session.delete(client)
+            deletions += 1
+        try:
+            db.session.commit()
+        except Exception, e:
+            db.session.rollback()
+            app.logger.error("Could not cleanup expired oauth2clients. "
+                             "Database error; rolled back: {0}".format(e))
+        app.logger.info("Deleted {0} oauth2clients whose last_activity was "
+                        "at least {1} old".format(deletions, timedelta))
 
 
--- adsws/tests/test_commands.py
+++ adsws/tests/test_commands.py
@@ -8,7 +8,7 @@ from adsws.modules.oauth2server.models import OAuthClient, OAuthToken
 from adsws.core import db, user_manipulator
 from adsws import factory
 from adsws.testsuite import make_test_suite, run_test_suite
-from adsws.accounts.manage import cleanup_tokens
+from adsws.accounts.manage import cleanup_tokens, cleanup_clients
 
 
 class TestManage_Accounts(TestCase):
@@ -27,9 +27,44 @@ class TestManage_Accounts(TestCase):
         )
         db.session.add(u)
 
+        now = datetime.datetime.now()
+        delta = datetime.timedelta
+        times = [
+            now,
+            now-delta(seconds=3),
+            now+delta(seconds=3),
+            now+delta(hours=1),
+        ]
+        self.times = times  # Save for comparisons in the tests
+
+        for _time in times:
+            client = OAuthClient(
+                user_id=u.id,
+                client_id=gen_salt(20),
+                client_secret=gen_salt(20),
+                is_confidential=False,
+                is_internal=True,
+                _default_scopes="",
+                last_activity=_time,
+            )
+            db.session.add(client)
+
+            token = OAuthToken(
+                client_id=client.client_id,
+                user_id=u.id,
+                access_token=gen_salt(20),
+                refresh_token=gen_salt(20),
+                expires=_time,
+                _scopes="",
+                is_personal=False,
+                is_internal=True,
+            )
+            db.session.add(token)
+
+        # Add a client without a last_activity to verify that the cleanup
+        # scripts do not break under this condition
         client = OAuthClient(
             user_id=u.id,
-            name="user-client",
             client_id=gen_salt(20),
             client_secret=gen_salt(20),
             is_confidential=False,
@@ -38,30 +73,19 @@ class TestManage_Accounts(TestCase):
         )
         db.session.add(client)
 
-        now = datetime.datetime.now()
-        delta = datetime.timedelta
-        expires = [
-            now,  # already expired
-            now-delta(seconds=3),  # already expired
-            now+delta(seconds=3),   # expired only after time.sleep(5)
-            now+delta(hours=1),  # will not be expired
-        ]
-        self.expires = expires  # Save for comparisons in the tests
-
-        for expiry in expires:
-            token = OAuthToken(
-                client_id=client.client_id,
-                user_id=u.id,
-                access_token=gen_salt(20),
-                refresh_token=gen_salt(20),
-                expires=expiry,
-                _scopes="",
-                is_personal=False,
-                is_internal=True,
-            )
-            db.session.add(token)
+        # Add a token without an expiry to verify that the cleanup scripts
+        # do not break under this condition
+        token = OAuthToken(
+            client_id=client.client_id,
+            user_id=u.id,
+            access_token=gen_salt(20),
+            refresh_token=gen_salt(20),
+            _scopes="",
+            is_personal=False,
+            is_internal=True,
+        )
+        db.session.add(token)
         db.session.commit()
-
 
     def create_app(self):
         app = factory.create_app(
@@ -71,42 +95,81 @@ class TestManage_Accounts(TestCase):
         )
         return app
 
-
     def test_token_cleanup(self):
         """
-        Tests that expires oauth2tokens are properly removed from the database
-        as a result of the procedure
+        Tests that expired oauth2tokens are properly removed from the database
+        as a result of the cleanup_token procedure
         """
         original_tokens = db.session.query(OAuthToken).all()
+        self.assertEqual(5, len(original_tokens))
         self.assertItemsEqual(
-            [i.expires for i in original_tokens],
-            self.expires
+            filter(None, [i.expires for i in original_tokens]),
+            self.times,
         )
 
+        # Only those tokens which have already expired should be removed
+        cleanup_tokens(app=self.app)
+        current_tokens = db.session.query(OAuthToken).all()
+        self.assertNotEqual(original_tokens, current_tokens)
+        self.assertEqual(3, len(current_tokens))
+        self.assertEqual(
+            filter(None, [i.expires for i in current_tokens]),
+            [i for i in self.times if i >= datetime.datetime.now()],
+        )
+
+        # Sleep to let one token expire
+        # and check that this token has been removed after calling
+        # the cleanup_tokens script again
+        time.sleep(3)
         cleanup_tokens(app=self.app)
         current_tokens = db.session.query(OAuthToken).all()
         self.assertNotEqual(original_tokens, current_tokens)
         self.assertEqual(2, len(current_tokens))
         self.assertEqual(
-            [i.expires for i in current_tokens],
-            [i for i in self.expires if i >= datetime.datetime.now()],
+            filter(None, [i.expires for i in current_tokens]),
+            [i for i in self.times if i >= datetime.datetime.now()],
         )
 
-        time.sleep(3)  # Sleep to let one token expire
-
-        cleanup_tokens(app=self.app)
-        current_tokens = db.session.query(OAuthToken).all()
-        self.assertNotEqual(original_tokens, current_tokens)
-        self.assertEqual(1, len(current_tokens))
-        self.assertEqual(
-            [i.expires for i in current_tokens],
-            [i for i in self.expires if i >= datetime.datetime.now()],
+    def test_client_cleanup(self):
+        """
+        Tests that oauth2clients whose last_activity attribute are properly
+        removed from the database as a result of the cleanup_client procedure
+        """
+        original_clients = db.session.query(OAuthClient).all()
+        self.assertEqual(5, len(original_clients))
+        self.assertItemsEqual(
+            filter(None, [i.last_activity for i in original_clients]),
+            self.times,
         )
 
+        # No clients should be cleaned
+        cleanup_clients(app=self.app, timedelta="days=31")
+        current_clients = db.session.query(OAuthClient).all()
+        self.assertEqual(5, len(current_clients))
 
+        # Cleanup all clients that are older than 0 seconds from now()
+        cleanup_clients(app=self.app, timedelta="seconds=0")
+        current_clients = db.session.query(OAuthClient).all()
+        self.assertEqual(3, len(current_clients))
 
+        # Wait 3 seconds, then perform the same cleanup. Should have one less
+        # client after this operation.
 
+        time.sleep(3.1)
+        cleanup_clients(app=self.app, timedelta="seconds=0.1")
+        current_clients = db.session.query(OAuthClient).all()
+        self.assertEqual(2, len(current_clients))
 
+        # Cleanup the client whose last_activity was set to 1 hour
+        # into the future. This case should never happen in practice!
+        cleanup_clients(app=self.app, timedelta="hours=-1")
+        current_clients = db.session.query(OAuthClient).all()
+        self.assertEqual(1, len(current_clients))
+
+        # Only the client with last_activity=None should remain
+        current_clients = db.session.query(OAuthClient).all()
+        self.assertEqual(1, len(current_clients))
+        self.assertIsNone(current_clients[0].last_activity)
 
 
 TEST_SUITE = make_test_suite(TestManage_Accounts)

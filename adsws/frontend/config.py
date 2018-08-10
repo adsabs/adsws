06a74aa012b31f2a768799568dde52e70f7886d9
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2018-08-10 14:31:19 -0400

    Updated adsmutils (#154)

--- adsws/frontend/config.py
+++ adsws/frontend/config.py
@@ -1,3 +1,6 @@
+LOG_LEVEL = 30 # To be deprecated when all microservices use ADSFlask
+LOGGING_LEVEL = "INFO"
+LOG_STDOUT = True
 EXTENSIONS = [
     'adsws.ext.sqlalchemy',
     'adsws.ext.security',
